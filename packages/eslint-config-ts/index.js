36ec052134b5e8ee1bb0c946527b1271f48d10e3
Feng Han <hi@fenghan.link>
2022-03-15 11:44:33 +0800

    refactor: refact ts config

---
+++ packages/eslint-config-ts/index.js
@@ -0,0 +1,98 @@
+module.exports = {
+  extends: ['@fh/eslint-config-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  overrides: require('@fh/eslint-config-base').overrides,
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      { 'ts-ignore': 'allow-with-description' },
+    ],
+    '@typescript-eslint/consistent-type-imports': ['error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: false,
+      },
+    ],
+    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      { allowSingleExtends: false },
+    ],
+    '@typescript-eslint/semi': ['error', 'never'],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      { multiline: { delimiter: 'none' } },
+    ],
+    '@typescript-eslint/type-annotation-spacing': ['error', {}],
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': [
+      'error',
+      'stroustrup',
+      { allowSingleLine: true },
+    ],
+    'no-useless-constructor': 'off',
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        caughtErrors: 'none',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: true,
+        classes: false,
+        variables: true,
+      },
+    ],
+    'object-curly-spacing': 'off',
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: false,
+        enforceForJSX: true,
+      },
+    ],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': [
+      'error', {
+        allow: [
+          'private-constructors',
+          'protected-constructors',
+          'decoratedFunctions',
+        ],
+      }],
+  },
+}
