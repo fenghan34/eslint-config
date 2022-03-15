b25cc1d92cf32abf954e95e09b88972e50f168d1
Feng Han <hi@fenghan.link>
2022-03-15 11:46:22 +0800

    chore: use pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-  root: true,
-  overrides: [
-    {
-      files: ['*.js'],
-      extends: '@fenghan',
-    },
-    {
-      files: ['*.ts', '*.tsx', '*.vue'],
-      extends: ['@fenghan/eslint-config-react', '@fenghan/eslint-config-vue'],
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-    },
-  ],
+  extends: ['@fh'],
 }
--- .stylelintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: '@fenghan/stylelint-config',
-}
